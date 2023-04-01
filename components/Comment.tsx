9666678dd7998b0ea082b67781f47d6691ba9dc0
leejongmin <60211689@mju.ac.kr>
2023-04-01 20:30:34 +0900

    giscus darkmode

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -1,6 +1,8 @@
 import Giscus from "@giscus/react";
+import { useTheme } from "next-themes";
 
 const Comment = () => {
+  const { theme, setTheme } = useTheme();
   return (
     <div className="border-t-[100px] border-transparent">
       <Giscus
@@ -13,7 +15,7 @@ const Comment = () => {
         reactionsEnabled="1"
         emitMetadata="0"
         inputPosition="top"
-        theme="light"
+        theme={theme === "light" ? "light" : "dark_dimmed"}
         lang="ko"
         loading="lazy"
       />
