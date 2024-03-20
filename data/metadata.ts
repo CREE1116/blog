0bd86a152d6a4a771739c4e67971fd11b43332e4
Lee_jongmin <104889277+CREE1116@users.noreply.github.com>
2024-03-20 11:34:29 +0900

    metadata.ts 업데이트

--- data/metadata.ts
+++ data/metadata.ts
@@ -3,7 +3,7 @@ const metadata = {
   description: "간단한 개발을 하는 크리의 블로그 입니다",
   site_name: "[크리의 끄적끄적]",
   author: "Cree",
-  siteUrl: "https://cree-devblog.net",
+  siteUrl: "https://cree-devblog.kro.kr",
   email: "leejongmin774@gmail.com",
 };
 
